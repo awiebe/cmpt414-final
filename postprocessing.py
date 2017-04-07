3c2703c55f414261441c65d21231aab7d2cb1e0d
Abram Wiebe <awiebe@burningthumb.com>
2017-04-07 05:01:32 -0700

    Corrected unit pricing

--- postprocessing.py
+++ postprocessing.py
@@ -52,33 +52,49 @@ for t in ysort:
         curY=t.position[0][1]
         curLine=t.content
 
+
+itemlines=[]
+prevItemLine=None
+
 for l in lines:
-    print l
     priceRegex= r'[$]*[ ]*([0-9]*)[\.]*([0-9]*)'
     itemLineRegex =r'([A-z ]*)[ ]* ([0-9]*)[ ]*[$]*[ ]*([0-9]*)[\.]*([0-9]*)'
-    unitLineRegex =r'([0-9]+)([lbgkgLmL]+) @ ([0-9]*)[\.]*([0-9]*)/[lbgkgLmL]* [$]*[ ]*([0-9]*)[\.]*([0-9]*)'
+    unitLineRegex =r'([0-9])([A-z]+)[^0-9\.]*[ ]*([0-9]*)\.([0-9]+)\/[^0-9\. ]*([0-9]*)[ ]*\.(.*)'
     
     #Tesserect can't distinguish between 1 and L very well. so sanitize the pounds unit
     tesseractSantized = l[1].replace("1b","lb")
+    print tesseractSantized
 
     searchUnit = re.search( unitLineRegex,tesseractSantized , re.M|re.I)
     searchItem = re.search( itemLineRegex, tesseractSantized, re.M|re.I)
+
     if searchUnit:
         
-        print "searchObj.group():", searchItem.group()
-        print "Unit quantity:", searchItem.group(1)
-        print "Units:", searchItem.group(2)
-        print "Unit Dollars: ", searchItem.group(2)
-        print "Unit Cents : ", searchItem.group(3)
-        print "Total Dollars: ", searchItem.group(4)
-        #print "Total Cents : ", searchItem.group(5)
+        print "Unit quantity:", searchUnit.group(1)
+        print "Units:", searchUnit.group(2)
+        print "Unit Dollars: ", searchUnit.group(3)
+        print "Cent suffix:", searchUnit.group(4).replace(" ","")
+        print "Total Dollars: ", searchUnit.group(5)
+        print "Total Cents : ", searchUnit.group(6)
     elif searchItem:
-        print "searchObj.group():", searchItem.group()
+        if searchItem.group(1) is None or searchItem.group(1) =="":
+            continue
+
         print "Item label:", searchItem.group(1)
         print "SKU (optional):", searchItem.group(2)
         print "Dollars: ", searchItem.group(3)
-        print "Cents : ", searchItem.group(4)
+        print "Cents :", searchItem.group(4).replace(" ","")
     else:
         print "Nothing found!!"
+    
+    print ""
 
     
+    class ItemLine:
+
+        def __init__(self,label,sku,totalPrice,unitqty,unitprice):
+            self.label=label
+            self.sku=sku
+            self.totalPrice=totalPrice
+            self.unitqty=unitqty
+            self.unitprice=unitprice
